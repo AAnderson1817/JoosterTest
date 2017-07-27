2e91a445afe45d67722a060c7390175aec955265
Andy Anderson <andyanderson1818@gmail.com>
2017-07-26 22:58:56 -0700

    Fixed overflow using overflow-x

--- client/js/main.js
+++ client/js/main.js
@@ -14,6 +14,45 @@ function closePanel(){
   successpanel.addClass('hidden');
 }
 
+//So that our checkSlide function doesn't trigger like crazy, we use debounce to limit the number of times a call can be made over a certain timeframe. Currently, any function wrapped in debounce can only fire once every 20 ms, as defined by the 'wait' argument. I've since reduced the wait time to 5ms in order to smooth the transition.
+
+
+// function debounce(func, wait = 5, immediate = true) {
+//   var timeout;
+//   return function() {
+//     var context = this, args = arguments;
+//     var later = function() {
+//       timeout = null;
+//       if (!immediate) func.apply(context, args);
+//     };
+//     var callNow = immediate && !timeout;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//     if (callNow) func.apply(context, args);
+//   };
+// }
+//BEGIN SLIDE IN FUNCTION
+
+//const sliderItems = document.querySelectorAll('.slide-in')
+
+// function checkSlide(e){
+//   sliderItems.forEach(sliderItem => {
+//     //Halfway through the image
+//     const slideInAt =(window.scrollY + window.innerHeight) - sliderItem.height / 10;
+//     //Bottom of the image
+//     const itemBottom = sliderItem.offsetTop + sliderItem.height;
+//     const isHalfShown = slideInAt > sliderItem.offsetTop;
+//     const isNotScrolledPast = window.scrollY < itemBottom;
+//     if(isHalfShown && isNotScrolledPast) {
+//       sliderItem.classList.add('active');
+//     } else{
+//       sliderItem.classList.remove('active');
+//     }
+//   });
+// }
+
+
+
 //Event Listeners
 signup.on('click', swapPanels);
 successexit.on('click', closePanel);
